25c885104cf0a5577a44bb98ac452edc3b20ed0d
Oz Deutsch <ozdeutsch1980@gmail.com>
2020-08-06 19:17:06 +0300

    Initial Commit

---
+++ VideoTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoTest
+//
+//  Created by Oz Deutsch on 06/08/2020.
+//  Copyright © 2020 Oz Deutsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
