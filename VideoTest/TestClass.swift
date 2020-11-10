b511b628dcb54d50f2fba29b386229f5b1e5be7f
OzdGini <ozd@gini-apps.com>
2020-11-10 18:40:35 +0200

    test added

---
+++ VideoTest/TestClass.swift
@@ -0,0 +1,24 @@
+//
+//  TestClass.swift
+//  CLDVideoView
+//
+//  Created by Oz Deutsch on 04/11/2020.
+//  Copyright © 2020 Cloudinary. All rights reserved.
+//
+
+import XCTest
+
+class TestClass: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssert(false, "this is a failed test")
+    }
+}
